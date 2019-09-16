c16c680c60b4e0296122b3c688a72848b4418017
Luca Marcelli <luca.marcelli@infoguard.ch>
2019-09-16 16:54:09 +0200

    more signature (too verbose still)

--- katana/gadgets.py
+++ katana/gadgets.py
@@ -1,5 +1,7 @@
 from katana import utils
 import pydis
+import threading
+import concurrent.futures
 
 class Gadgets():
     def __init__(self):
@@ -28,15 +30,57 @@ class Gadgets():
         self.gadgets.extend(_gadgets)
     
     def match_jump(self, addr, depth):
-        if self.bytes[addr - 1] != 0x00 and self.bytes[addr - 1] != 0x30 and self.bytes[addr] == 0xc3: # ret
+        if self.bytes[addr] == 0xc3: # ret
             self.resolve(addr, depth)
         elif self.bytes[addr] == 0x0f and self.bytes[addr + 1] == 0x05: # syscall
             self.resolve(addr + 1, depth)
+        elif self.bytes[addr] == 0x41 and self.bytes[addr + 1] == 0xff: # call [rX]
+            self.resolve(addr + 2, depth)
+            self.resolve(addr + 3, depth)
+        elif self.bytes[addr] == 0xff and self.bytes[addr + 1] == 0xd0: # call rax
+            self.resolve(addr + 1, depth)
+        elif self.bytes[addr] == 0xff and self.bytes[addr + 1] == 0xd1: # call rcx
+            self.resolve(addr + 1, depth)
+        elif self.bytes[addr] == 0xff and self.bytes[addr + 1] == 0xd2: # call rdx
+            self.resolve(addr + 1, depth)
+        elif self.bytes[addr] == 0xff and self.bytes[addr + 1] == 0xd3: # call rbx
+            self.resolve(addr + 1, depth)
+        elif self.bytes[addr] == 0xff and self.bytes[addr + 1] == 0xd4: # call rsp
+            self.resolve(addr + 1, depth)
+        elif self.bytes[addr] == 0xff and self.bytes[addr + 1] == 0xd5: # call rbp
+            self.resolve(addr + 1, depth)
+        elif self.bytes[addr] == 0xff and self.bytes[addr + 1] == 0xd6: # call rsi
+            self.resolve(addr + 1, depth)
+        elif self.bytes[addr] == 0xff and self.bytes[addr + 1] == 0xd7: # call rdi
+            self.resolve(addr + 1, depth)
+        elif self.bytes[addr] == 0x41 and self.bytes[addr + 1] == 0xff and self.bytes[addr + 2] == 0xd0: # call r8
+            self.resolve(addr + 2, depth)
+        elif self.bytes[addr] == 0x41 and self.bytes[addr + 1] == 0xff and self.bytes[addr + 2] == 0xd1: # call r9
+            self.resolve(addr + 2, depth)
+        elif self.bytes[addr] == 0x41 and self.bytes[addr + 1] == 0xff and self.bytes[addr + 2] == 0xd2: # call r10
+            self.resolve(addr + 2, depth)
+        elif self.bytes[addr] == 0x41 and self.bytes[addr + 1] == 0xff and self.bytes[addr + 2] == 0xd3: # call r11
+            self.resolve(addr + 2, depth)
+        elif self.bytes[addr] == 0x41 and self.bytes[addr + 1] == 0xff and self.bytes[addr + 2] == 0xd4: # call r12
+            self.resolve(addr + 2, depth)
+        elif self.bytes[addr] == 0x41 and self.bytes[addr + 1] == 0xff and self.bytes[addr + 2] == 0xd5: # call r13
+            self.resolve(addr + 2, depth)
+        elif self.bytes[addr] == 0x41 and self.bytes[addr + 1] == 0xff and self.bytes[addr + 2] == 0xd6: # call r14
+            self.resolve(addr + 2, depth)
+        elif self.bytes[addr] == 0x41 and self.bytes[addr + 1] == 0xff and self.bytes[addr + 2] == 0xd7: # call r15
+            self.resolve(addr + 2, depth)
+        elif self.bytes[addr] == 0xff: # call [reg]
+            self.resolve(addr + 2, depth)
+            self.resolve(addr + 3, depth)
     
     def find_all(self, depth=6):
         self.gadgets = []
+        """with concurrent.futures.ThreadPoolExecutor(max_workers=50) as executor:
+            for i in range(0, len(self.bytes)):
+                executor.submit(self.match_jump, i, depth)
+            executor.shutdown(wait=True)"""
         for i in range(0, len(self.bytes)):
-            self.match_jump(i, depth=depth)
+            self.match_jump(i, depth)
         self.clean()
     
     def clean(self):
@@ -46,7 +90,7 @@ class Gadgets():
             instructions = pydis.decode(gadget, pointer)
             for instruction in instructions:
                 gadgets.append(str(instruction))
-            if len(gadgets) > 0 and (gadgets[-1].startswith("ret") or gadgets[-1].startswith("syscall")):
+            if len(gadgets) > 0 and (gadgets[-1].startswith("ret") or gadgets[-1].startswith("syscall") or gadgets[-1].startswith("call")):
                 cleaned.append((gadget, pointer))
         self.gadgets = cleaned
         self.remove_duplicates()
--- main.py
+++ main.py
@@ -4,6 +4,6 @@ import sys
 if __name__ == "__main__":
     _gadgets = Gadgets()
     _gadgets.load_file(sys.argv[1])
-    _gadgets.find_all(depth=10)
+    _gadgets.find_all(depth=8)
     print(_gadgets)
     print("Found %i gadgets!" % len(_gadgets))
