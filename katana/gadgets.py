4538dc56f4f202b51d8d5a826cad45b677f8a235
Luca Marcelli <luca.marcelli@infoguard.ch>
2019-09-16 17:19:39 +0200

    improved signature scan

--- katana/gadgets.py
+++ katana/gadgets.py
@@ -1,4 +1,5 @@
 from katana import utils
+from katana.signatures import *
 import pydis
 import threading
 import concurrent.futures
@@ -8,6 +9,11 @@ class Gadgets():
         self.bytes = []
         self.gadgets = []
         self.is_x64 = True
+        self.signatures = [
+            RET, SYSCALL,
+            CALL_RAX, CALL_RBP, CALL_RBX, CALL_RCX, CALL_RDI, CALL_RDX, CALL_RSI, CALL_RSP,
+            CALL_R8, CALL_R9, CALL_R10, CALL_R11, CALL_R12, CALL_R13, CALL_R14, CALL_R15
+        ]
 
     def load_file(self, path):
         with open(path, "rb") as file:
@@ -30,52 +36,17 @@ class Gadgets():
         self.gadgets.extend(_gadgets)
     
     def match_jump(self, addr, depth):
-        if self.bytes[addr] == 0xc3: # ret
-            self.resolve(addr, depth)
-        elif self.bytes[addr] == 0x0f and self.bytes[addr + 1] == 0x05: # syscall
-            self.resolve(addr + 1, depth)
-        elif self.bytes[addr] == 0x41 and self.bytes[addr + 1] == 0xff: # call [rX]
-            self.resolve(addr + 2, depth)
-            self.resolve(addr + 3, depth)
-        elif self.bytes[addr] == 0xff and self.bytes[addr + 1] == 0xd0: # call rax
-            self.resolve(addr + 1, depth)
-        elif self.bytes[addr] == 0xff and self.bytes[addr + 1] == 0xd1: # call rcx
-            self.resolve(addr + 1, depth)
-        elif self.bytes[addr] == 0xff and self.bytes[addr + 1] == 0xd2: # call rdx
-            self.resolve(addr + 1, depth)
-        elif self.bytes[addr] == 0xff and self.bytes[addr + 1] == 0xd3: # call rbx
-            self.resolve(addr + 1, depth)
-        elif self.bytes[addr] == 0xff and self.bytes[addr + 1] == 0xd4: # call rsp
-            self.resolve(addr + 1, depth)
-        elif self.bytes[addr] == 0xff and self.bytes[addr + 1] == 0xd5: # call rbp
-            self.resolve(addr + 1, depth)
-        elif self.bytes[addr] == 0xff and self.bytes[addr + 1] == 0xd6: # call rsi
-            self.resolve(addr + 1, depth)
-        elif self.bytes[addr] == 0xff and self.bytes[addr + 1] == 0xd7: # call rdi
-            self.resolve(addr + 1, depth)
-        elif self.bytes[addr] == 0x41 and self.bytes[addr + 1] == 0xff and self.bytes[addr + 2] == 0xd0: # call r8
-            self.resolve(addr + 2, depth)
-        elif self.bytes[addr] == 0x41 and self.bytes[addr + 1] == 0xff and self.bytes[addr + 2] == 0xd1: # call r9
-            self.resolve(addr + 2, depth)
-        elif self.bytes[addr] == 0x41 and self.bytes[addr + 1] == 0xff and self.bytes[addr + 2] == 0xd2: # call r10
-            self.resolve(addr + 2, depth)
-        elif self.bytes[addr] == 0x41 and self.bytes[addr + 1] == 0xff and self.bytes[addr + 2] == 0xd3: # call r11
-            self.resolve(addr + 2, depth)
-        elif self.bytes[addr] == 0x41 and self.bytes[addr + 1] == 0xff and self.bytes[addr + 2] == 0xd4: # call r12
-            self.resolve(addr + 2, depth)
-        elif self.bytes[addr] == 0x41 and self.bytes[addr + 1] == 0xff and self.bytes[addr + 2] == 0xd5: # call r13
-            self.resolve(addr + 2, depth)
-        elif self.bytes[addr] == 0x41 and self.bytes[addr + 1] == 0xff and self.bytes[addr + 2] == 0xd6: # call r14
-            self.resolve(addr + 2, depth)
-        elif self.bytes[addr] == 0x41 and self.bytes[addr + 1] == 0xff and self.bytes[addr + 2] == 0xd7: # call r15
-            self.resolve(addr + 2, depth)
-        elif self.bytes[addr] == 0xff: # call [reg]
-            self.resolve(addr + 2, depth)
-            self.resolve(addr + 3, depth)
+        for signature in self.signatures:
+            matched = False
+            for i in range(0, len(signature)):
+                if addr + i >= len(self.bytes): break
+                matched = self.bytes[addr + i] == signature[i]
+            if matched:
+                self.resolve(addr + len(signature) - 1, depth)
     
     def find_all(self, depth=6):
         self.gadgets = []
-        """with concurrent.futures.ThreadPoolExecutor(max_workers=50) as executor:
+        """with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
             for i in range(0, len(self.bytes)):
                 executor.submit(self.match_jump, i, depth)
             executor.shutdown(wait=True)"""
